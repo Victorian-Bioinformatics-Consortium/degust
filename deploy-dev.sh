be44dec247810d85ccc7d6451bfd714f3e3c90a9
David Powell <david.powell@monash.edu>
2013-04-12 11:57:13 +1000

    Add a script for deploying to apache

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+dest=dist-dev
+
 (cd coffee ; coffee -c .)
 
-mkdir -p dist
-cd dist
+mkdir -p "$dest"
+cd "$dest"
 
 ln -sf ../src/r-json.hs r-json.cgi
 
@@ -12,4 +14,6 @@ for f in ../html/* ../src/* ../coffee/*.js ../kegg; do ln -sf $f .; done
 mkdir -p tmp
 mkdir -p cached
 mkdir -p user-files
+
+echo "Linked files to '$dest'.  Now run ./server.py"
 
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import CGIHTTPServer, BaseHTTPServer
 
 
 os.system("./deploy-dev.sh")
-os.chdir("dist")
+os.chdir("dist-dev")
 
 
 
