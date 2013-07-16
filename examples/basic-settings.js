5e3187580950c1b824f917661a26520d4cddb0a5
David Powell <david@drp.id.au>
2013-07-16 22:35:40 +1000

    Put example settings js in place

---
+++ examples/basic-settings.js
@@ -0,0 +1,12 @@
+window.settings = {
+  name: "Basic Example",
+  csv: "basic.csv",
+  csv_format: true,
+
+  skip: 0,
+  id_column: 0,
+  column_names: ["id","ABS.1","ABS.2","AveExpr","adj.P.Val"],
+  info_columns: [0],
+  replicates: [[0,[]], [1,[]]],
+  replicate_names: ["Cond 1", "Cond 2"],
+};
