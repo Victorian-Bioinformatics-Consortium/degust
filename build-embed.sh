eb85a9d9f0601d15867e81301a89626b1c09369e
David Powell <david@drp.id.au>
2014-03-19 16:41:55 +1100

    Create build-embed.sh to build the degust.py file

---
+++ build-embed.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Exit if any command fails
+set -e
+
+degust_home='http://victorian-bioinformatics-consortium.github.io/degust'
+
+case "$1" in
+    local)
+        url='http://localhost:8000/'
+        echo "Building LOCAL, ensure you run ./build.sh dev"
+        ;;
+    remote)
+        url='http://victorian-bioinformatics-consortium.github.io/degust/dist/latest/'
+        ;;
+    *)
+      echo "Usage: ./build-embed.sh local|remote"
+      exit 1
+      ;;
+esac
+
+ver=$(grep ver app/js/version.coffee | sed -e 's/^.* = //')
+ver="${ver//\'}"
+
+echo "Building v$ver.  Using URL $url"
+
+if [ -z "$ver" ]; then
+  echo "Version not found!"
+  exit 1
+fi
+
+sed -e "s|'\./|'$url|" \
+    -e s"|index\.html|$degust_home|" app/html/compare.html > xx.html
+
+
+
+sed -e "/HTML-HERE/r xx.html" \
+    -e '/HTML-HERE/d' \
+    -e "s/VERSION-HERE/$ver/g" \
+    -e "s^ASSET-HERE^$url^g"  app/scripts/embed.py > build/degust.py
+
