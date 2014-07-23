d419a92c5275dd601d377b3e059a80a1e3ad280b
David Powell <david@drp.id.au>
2014-07-23 16:20:58 +1000

    Fix build-watchify

--- build-watchify.sh
+++ build-watchify.sh
@@ -3,7 +3,7 @@
 dest=build
 
 for f in app/js/*-req.coffee; do
-    t="$dest/"$(basename "${f%-req.coffee}")".js"
+    t="$dest/public/"$(basename "${f%-req.coffee}")".js"
     echo "Watching $f -> $t"
     watchify --debug -t coffeeify -t hbsfy $f -o $t -v &
 done
